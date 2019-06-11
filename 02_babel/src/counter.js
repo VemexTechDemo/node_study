ba284e6303827a75ddb423fd00dadaff596e15b5
王巍巍 <vivlanwong120729@gmail.com>
2019-06-11 15:43:35 +0800

    babel demo

---
+++ 02_babel/src/counter.js
@@ -0,0 +1,6 @@
+var count=0;
+var countFun=function(){
+    count++;
+};
+export {count}
+export {countFun}
---
+++ 02_babel/src/index.js
@@ -0,0 +1,4 @@
+import {count,countFun} from "./counter";
+console.log(count);
+countFun();
+console.log(count);
